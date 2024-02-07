e0b56eb548312f8fd622a6d472b166a3b2b0cd33
UrKo <urko2376@googlemail.com>
2024-02-07 12:04:19 +0000

    Added code for menu iotem in base.html, added main.css, added, logo image, added. main.js

--- essential_events/settings.py
+++ essential_events/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'essential_events_main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ essential_events_main/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EssentialEventsMainConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'essential_events_main'
